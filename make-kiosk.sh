be71d985460de098f5f735688137fb661ac05378
Draco Chau <draco.chau@gmail.com>
2019-01-10 09:18:11 +0000

    .

--- make-kiosk.sh
+++ make-kiosk.sh
@@ -2,4 +2,17 @@
 
 echo $1;
 
+echo "===Remove Unwanted Packages==="
+sudo apt-get install xdotool unclutter sed
+
+sudo apt-get purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 -y
+sudo apt-get purge smartsim java-common minecraft-pi libreoffice* -y
+
+sudo apt-get clean
+sudo apt-get autoremove -y
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+
 
