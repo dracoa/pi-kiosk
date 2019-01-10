12a00682f766b7439d2496d457d4ecaf1b20e1ab
Draco Chau <draco.chau@gmail.com>
2019-01-10 10:29:53 +0000

    ,

--- make-agent.sh
+++ make-agent.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]
+  then
+    echo "Usage: make-agent.sh [hostname] [eap-password]"
+    echo "Excample: make-agent.sh ABC01 1234"
+    exit 1
+fi
+
 sudo apt-get install git time fping dnsutils zabbix-agent -y
 
 sudo cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.bak
 sudo sed -i 's/Server=127.0.0.1/Server=144.214.93.106/g' /etc/zabbix/zabbix_agentd.conf
 sudo sed -i 's/ServerActive=127.0.0.1/ServerActive=144.214.93.106/g' /etc/zabbix/zabbix_agentd.conf
-sudo sed -i 's/Hostname=Zabbix server/Hostname=sro-sg03/g' /etc/zabbix/zabbix_agentd.conf
+sudo sed -i -e "s/Hostname=Zabbix server/Hostname=$1/g" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i 's/# Timeout=3/Timeout=30/g' /etc/zabbix/zabbix_agentd.conf
 
-sudo wget -O /etc/zabbix/zabbix_agentd.conf.d/sro-wifi.conf 
+sudo wget -O /etc/zabbix/zabbix_agentd.conf.d/sro-wifi.conf https://raw.githubusercontent.com/dracoa/pi-kiosk/master/sro-wifi.conf
 
 sudo systemctl restart zabbix-agent
 
